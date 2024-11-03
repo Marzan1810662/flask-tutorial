2b091853ae062a5565eba6d87e5ff5ac0cbbfb5e
Marzan1810662 <hassanmarzan17@gmail.com>
2024-11-03 10:43:08 +0600

    routes and urls

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
-from flask import Flask
+from flask import Flask, request,make_response
 
 app =Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return "<h1>Hello World</h1>"
+
+
+@app.route('/hello', methods = ['GET', 'POST'])
+def hello():
+    if request.method == 'GET':
+        return 'You have made a get request', 200
+    elif request.method == 'POST':
+        return 'You have a POST request'
+    else: return "If methods are fixed you will probably never see this BUT Hello There"
+
+@app.route('/response-crafting')
+def response_crafting():
+    response = make_response('the is the response')
+    response.status_code = 00
+    response.headers['content-type'] = 'plain/text'
+    return response
+
+# Dynamic URLs
+@app.route('/greet/<name>')
+def greet(name):
+    return f"Hello {name}"
+
+@app.route('/add/<int:number1>/<int:number2>')
+def add(number1, number2):
+    return f'{number1} + {number2} = {number1+number2}'
+    
+# URL Parameters
+# to handle url parameters I need to import the request object from flask
+@app.route('/handle_url_params')
+def handle_params():
+    # return str(request.args)
+    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
+        greeting = request.args['greeting']
+        name = request.args['name']
+        return f'{greeting}, {name}'
+    else:
+        return "Some parameters are missing"
 
 
 if __name__ == '__main__':
