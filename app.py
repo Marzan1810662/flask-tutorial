b465171c97bb604e31b19d572c3e67783c36f1f3
marzan <hassanmarzan17@gmail.com>
2024-11-03 16:11:31 +0600

    redirect dynamic url

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request,make_response,render_template
+from flask import Flask, request,make_response,render_template,redirect, url_for
 
 app =Flask(__name__, template_folder='templates')
 
@@ -26,6 +26,10 @@ def repeat(s, times= 2):
 @app.template_filter('alternate_case')
 def alternate_case(s):
     return ''.join([c.upper() if i % 2 == 0 else c.lower() for i,c in enumerate(s)])
+
+@app.route('/redirect_endpoint')
+def redirect_endpoint():
+    return redirect(url_for('other'))
 
 # @app.route('/')
 # def index():
